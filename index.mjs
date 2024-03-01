a2fe2d3152566d462a487088ee8b80d5dce0ff8e
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-03-01 12:22:56 +0500

    add some changeeee

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes/index.mjs';
-import db  from './config/db.mjs'
+import db  from './config/db.mjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-
+const port = process.env.PORT || 3001;
+console.log(port);
 db.connection.once('open', () => {
     console.log("Db is connected");
 })
@@ -11,7 +15,7 @@ db.connection.once('open', () => {
     console.log("error", error.message);
 });
 
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log('Server is responding');
 });
 
