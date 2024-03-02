ab725ada7c4b3efb55bbb9ca29b80f9d584a7933
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-03-02 11:55:28 +0500

    add product id work

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,11 +1,13 @@
 import express from 'express';
 import products from './products.mjs';
+import productId from './productId.mjs';
 import user from './user.mjs';
 import UserInfo from './userInfo.mjs';
 
 const router = express.Router();
 
 router.use('/products', products);
+router.use('/productid', productId);
 router.use('/user', user);
 router.use('/userinfo', UserInfo);
 
