726bc95f2857401dfc41ef3ed9631520c64ffb1f
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-03-02 22:55:43 +0500

    add some image work

--- routes/imageToLink.mjs
+++ routes/imageToLink.mjs
@@ -1,12 +1,16 @@
 import express from 'express';
+import imageConversion from 'image-conversion'
 
 const router = express.Router();
 
 router.get('/:img', async (req, res) => {
     const { params: { img : file } } = req;
     console.log(file);
-    const res = await imageConversion.compressAccurately(file,200)
-    console.log(res);
+    const data = await imageConversion.compressAccurately(file,200);
+
+    console.log(data);
+
+    res.json({msg: "image convert successfully", url: data})
 });
 
 export default router;
