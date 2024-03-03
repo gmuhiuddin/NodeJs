34644187a614a4eaa915fc14768ea4cc8e26b971
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-03-03 22:45:39 +0500

    add soemcajdfhlkjasdflj

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,13 +3,11 @@ import products from './products.mjs';
 import productId from './productId.mjs';
 import user from './user.mjs';
 import UserInfo from './userInfo.mjs';
-import imgToUrl from './imageToLink.mjs';
 
 const router = express.Router();
 
 router.use('/products', products);
 router.use('/productid', productId);
-router.use('/imgtourl', imgToUrl);
 router.use('/user', user);
 router.use('/userinfo', UserInfo);
 
