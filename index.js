86880b1bc08088bc0c3378b9371046778330c12d
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-04-30 16:05:22 +0500

    add some changes in stg

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
-import routes from './routes/index.js';
-import db  from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import routes from './routes/index.js';
+import db  from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
 
 db.connection.once('open', () => {
     console.log("Db is connected");
@@ -17,13 +21,8 @@ db.connection.once('open', () => {
     console.log("error", error.message);
 });
 
-app.use(cors());
-app.use(cookieParser());
-
 app.listen(port, () => {
     console.log('Server is responding');
 });
-
-app.use(express.json());
 
 app.use('/', routes);
