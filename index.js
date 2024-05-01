13468d861e0bdcc559a0402e8b2fe395a44c87a9
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-05-01 12:09:37 +0500

    add change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes/index.js';
-import db  from './config/db.js';
+import db from './config/db.js';
 
 dotenv.config();
 
@@ -13,15 +13,15 @@ const port = process.env.PORT || 3001;
 db.connection.once('open', () => {
     console.log("Db is connected");
 })
-.on('error', (error) => {
-    console.log("error", error.message);
-});
+    .on('error', (error) => {
+        console.log("error", error.message);
+    });
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ['https://olx-clone-b4869.web.app/','http://localhost:5173'],
-  credentials: true
+    origin: [ 'http://localhost:5173'],
+    credentials: true
 }));
 
 app.listen(port, () => {
