9053308e058eb3a8a89e5e9e8ca8715691d92212
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-05-02 16:54:17 +0500

    add some asdfasdfasdf

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ db.connection.once('open', () => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
   credentials: true
 }));
 
